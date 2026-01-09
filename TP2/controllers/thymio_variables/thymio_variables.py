7bb4576a27966a8874b73e2db87b850668021311
Mathieu Oriol <mathieu.oriol@telecom-paris.fr>
2026-01-09 17:06:42 +0100

    TP2 I 7

--- TP2/controllers/thymio_variables/thymio_variables.py
+++ TP2/controllers/thymio_variables/thymio_variables.py
@@ -23,18 +23,19 @@ message through an emitter or handle the position of Robot1.
 ## https://www.cyberbotics.com/doc/guide/thymio2?version=develop
 
 import math
+import matplotlib.pyplot as plt
 
-from controller import Robot
+from controller import Supervisor
 
-robot = Robot()
+robot = Supervisor()
+node = robot.getFromDef("thymio")
 timestep = int(robot.getBasicTimeStep())
 keyboard = robot.getKeyboard()
 keyboard.enable(timestep)
 robot_speed = 0.0
 MAX_SPEED = 8
-e = 54 # mm
-
-wheel_radius = 21 # mm
+e = 54  # mm
+wheel_radius = 21  # mm
 
 print(chr(27) + "[2J")  # ANSI code for clearing command line
 print("Initialization of thymio_variables controller")
@@ -44,11 +45,58 @@ motor_right = robot.getDevice("motor.right")
 motor_left.setPosition(float("inf"))
 motor_right.setPosition(float("inf"))
 
+
 x_right = 0
 x_left = 0
-Xk = [0, 0, 0]
+Xk = [149.749, -156.634, -2.63059]
 
-def update_localisation():
+list_pos_x = []
+list_pos_y = []
+
+list_pos_x_sim = []
+list_pos_y_sim = []
+
+list_theta = []
+list_theta_sim = []
+
+
+def draw_pos():
+    plt.ion()
+    # create 3 subplots: x, y, theta
+    fig, axs = plt.subplots(3, 1, figsize=(6, 8))
+
+    t = range(len(list_pos_x))
+
+    # X
+    axs[0].plot(t, list_pos_x, label='estimate', color='blue')
+    axs[0].plot(t, list_pos_x_sim, label='reference', color='red', linestyle='--')
+    axs[0].set_ylabel('X (mm)')
+    axs[0].legend()
+    axs[0].grid(True)
+
+    # Y
+    axs[1].plot(t, list_pos_y, label='estimate', color='blue')
+    axs[1].plot(t, list_pos_y_sim, label='reference', color='red', linestyle='--')
+    axs[1].set_ylabel('Y (mm)')
+    axs[1].legend()
+    axs[1].grid(True)
+
+    # Theta (degrees)
+    axs[2].plot(t, list_theta, label='estimate', color='blue')
+    axs[2].plot(t, list_theta_sim, label='reference', color='red', linestyle='--')
+    axs[2].set_ylabel('Theta (deg)')
+    axs[2].set_xlabel('Timestep samples')
+    axs[2].legend()
+    axs[2].grid(True)
+
+    plt.tight_layout()
+    plt.draw()
+    plt.pause(0.0001)
+    plt.clf()
+    
+
+
+def update_pos():
     global x_left, x_right
     dleft = motor_left.getVelocity() * timestep * wheel_radius * 1e-3
     x_left += dleft
@@ -59,13 +107,32 @@ def update_localisation():
     # print(f"Deplacement total roue droite: {x_right} mm roue gauche: {x_left} mm")
 
     ds = (dleft + dright) / 2
-    dtheta = (dleft - dright) / (2 * e)
+    dtheta = (dright - dleft) / (2 * e)
     Xk[0] += ds * math.cos(Xk[2] + dtheta / 2)
     Xk[1] += ds * math.sin(Xk[2] + dtheta / 2)
     Xk[2] += dtheta
-
+    list_pos_x.append(Xk[0])
+    list_pos_y.append(Xk[1])
+    # store estimated theta in degrees for plotting
+    list_theta.append(math.degrees(Xk[2]))
     print(Xk)
 
+    ## Simulateur
+    xyz = node.getPosition()
+    rotation = node.getOrientation()
+    # rotation is a 3x3 matrix in row-major order
+    # yaw (theta) = atan2(r10, r00)
+    try:
+        yaw = math.atan2(rotation[3], rotation[0])
+    except Exception:
+        yaw = 0.0
+    # store simulator (reference) positions and theta in degrees
+    list_pos_x_sim.append(xyz[0] * 1e3)
+    list_pos_y_sim.append(xyz[1] * 1e3)
+    list_theta_sim.append(math.degrees(yaw))
+
+
+c = 0
 
 while robot.step(timestep) != -1:
     ## Controle clavier ##
@@ -93,5 +160,8 @@ while robot.step(timestep) != -1:
         motor_left.setVelocity(robot_speed)
         motor_right.setVelocity(robot_speed)
 
-    update_localisation()
- 
+    update_pos()
+    if c == 50:
+        draw_pos()
+        c = 0
+    c += 1
