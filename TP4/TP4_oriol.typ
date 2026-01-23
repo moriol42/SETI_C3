07151595caed45346c491b4e5b6e0e7ab36c36de
Mathieu Oriol <mathieu.oriol@telecom-paris.fr>
2026-01-23 14:44:57 +0100

    TP4 q1 et 2

---
+++ TP4/TP4_oriol.typ
@@ -0,0 +1,17 @@
+#import "@local/rapport:0.2.0": rapport
+
+// LTeX: language=fr
+#set text(lang: "fr")
+
+#show: rapport.with(
+  title: "TP4 C3 Navigation réactive",
+  subtitle: none,
+  authors: "Mathieu Oriol",
+  show_header: false,
+)
+
+= I. Télémètre laser - Follow the gap!
+
+
+= II. The bubble!
+
---
+++ TP4/controllers/thymio_variables/thymio_variables.py
@@ -0,0 +1,241 @@
+# Copyright 1996-2019 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This controller gives to its node the following behavior:
+Listen the keyboard. According to the pressed key, send a
+message through an emitter or handle the position of Robot1.
+"""
+
+
+## Reference
+## https://www.cyberbotics.com/doc/guide/thymio2?version=develop
+
+import math
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.transforms import Affine2D
+from scipy.spatial import KDTree
+
+from controller import Supervisor
+import numpy as np
+
+robot = Supervisor()
+node = robot.getFromDef("thymio")
+timestep = int(robot.getBasicTimeStep())
+keyboard = robot.getKeyboard()
+keyboard.enable(timestep)
+robot_speed = 0.0
+MAX_SPEED = 8
+e = 0.054  # m
+wheel_radius = 0.021  # m
+
+lidar = robot.getDevice("lidar")
+lidar.enable(timestep)
+lidar.enablePointCloud()
+
+HORZ_RES = lidar.getHorizontalResolution()
+
+print(chr(27) + "[2J")  # ANSI code for clearing command line
+print("Initialization of thymio_variables controller")
+
+motor_left = robot.getDevice("motor.left")
+motor_right = robot.getDevice("motor.right")
+motor_left.setPosition(float("inf"))
+motor_right.setPosition(float("inf"))
+
+
+def pos_robot():
+    """Return the exact position of the robot using the simulator values"""
+    x, y, _ = node.getPosition()
+    rotation = node.getOrientation()
+    theta = -math.atan2(rotation[3], rotation[0]) + math.pi / 2
+    return x, y, theta
+
+
+def base_robot2main(p):
+    """Changement de base du robot vers la base principal de la scene"""
+    # Apply the correction
+    x, y, theta = pos_robot()
+    proj = np.array(
+        [
+            math.cos(theta) * p[0] + math.sin(theta) * p[1] + x,
+            -math.sin(theta) * p[0] + math.cos(theta) * p[1] + y,
+        ]
+    )
+    return proj
+
+
+def proj_point_cloud(point_cloud):
+    angle = math.pi
+    point_cloud_xy = np.zeros((len(point_cloud), 2))
+    for i, p in enumerate(point_cloud):
+        xy = [p * math.sin(angle), p * math.cos(angle)]
+        point_cloud_xy[i] = base_robot2main(xy)
+        angle += 2 * math.pi / HORZ_RES
+    return point_cloud_xy
+
+
+# wall from webots config
+walls = [
+    {"translation": (-0.16, 0.23, 0), "size": (0.01, 1, 0.1)},
+    {"translation": (0.05, 0.73, 0), "size": (0.43, 0.01, 0.1)},
+    {"translation": (0.25, 0, 0), "size": (0.4, 0.01, 0.1)},
+    {
+        "translation": (0.0732246, -0.0387871, 0),
+        "rotation": 0.785398,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (0.631834, -0.215563, 0),
+        "rotation": 0.785398,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (0.0732248, 0.0319252, 0),
+        "rotation": 2.35619,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (0.631834, 0.2087, 0),
+        "rotation": 2.35619,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (0.242929, 0.696599, 0),
+        "rotation": 2.35619,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (-0.124765, 0.696599, 0),
+        "rotation": 0.785398,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (0.214645, -0.717604, 0),
+        "rotation": -2.3561953071795863,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (0.00251402, -0.47719, 0),
+        "rotation": -2.3561953071795863,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (-0.612664, -0.314557, 0),
+        "rotation": -2.3561953071795863,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {
+        "translation": (-0.612664, -0.710533, 0),
+        "rotation": 2.35619,
+        "size": (0.1, 0.01, 0.1),
+    },
+    {"translation": (-0.4, -0.27, 0), "size": (0.49, 0.01, 0.1)},
+    {"translation": (-0.16, -0.51, 0), "size": (0.4, 0.01, 0.1)},
+    {"translation": (0.46, -0.25, 0), "size": (0.43, 0.01, 0.1)},
+    {"translation": (-0.2, -0.75, 0), "size": (0.9, 0.01, 0.1)},
+    {"translation": (0.47, 0.25, 0), "size": (0.4, 0.01, 0.1)},
+    {"translation": (0.04, -0.01, 0), "size": (0.01, 1, 0.1)},
+    {"translation": (0.25, -0.5, 0), "size": (0.01, 0.5, 0.1)},
+    {"translation": (-0.65, -0.5, 0), "size": (0.01, 0.5, 0.1)},
+    {"translation": (0.27, 0.49, 0), "size": (0.01, 0.48, 0.1)},
+    {"translation": (0.67, 0, 0), "size": (0.01, 0.5, 0.1)},
+]
+
+
+def draw_point_cloud(point_cloud):
+    """Affiche le nuage de points du lidar"""
+    pc_simu = proj_point_cloud(point_cloud)
+    plt.ion()
+    if not hasattr(draw_point_cloud, "fig_ax") or draw_point_cloud.fig_ax is None:
+        fig, ax = plt.subplots()
+        draw_point_cloud.fig_ax = (fig, ax)
+        plt.show(block=False)
+    else:
+        fig, ax = draw_point_cloud.fig_ax
+
+    # Draw walls
+    ax.cla()
+    for w in walls:
+        tx, ty, _ = w["translation"]
+        sx, sy, _ = w["size"]
+        angle = w.get("rotation", 0)
+        rect = patches.Rectangle(
+            (-sx / 2, -sy / 2),
+            sx,
+            sy,
+            linewidth=1,
+            edgecolor="black",
+            facecolor="gray",
+            alpha=0.7,
+        )
+        transform = Affine2D().rotate(angle).translate(tx, ty) + ax.transData
+        rect.set_transform(transform)
+        ax.add_patch(rect)
+
+    plt.scatter(
+        pc_simu[:, 0],
+        pc_simu[:, 1],
+    )
+
+    ax.set_aspect("equal")
+    ax.set_xlabel("x (m)")
+    ax.set_ylabel("y (m)")
+    ax.grid(True)
+    ax.relim()
+    ax.autoscale_view()
+
+    plt.tight_layout()
+    fig.canvas.draw_idle()
+    plt.pause(0.01)
+
+c = 0
+
+while robot.step(timestep) != -1:
+    ## Lidar ##
+    point_cloud = lidar.getRangeImage()
+    if c == 100:
+        draw_point_cloud(point_cloud)
+        c = 0
+    c += 1
+
+    ## Controle clavier ##
+    command = keyboard.getKey()
+    if command == keyboard.LEFT:
+        # print('Left')
+        motor_left.setVelocity(robot_speed - 2)
+        motor_right.setVelocity(robot_speed + 2)
+        if abs(robot_speed) > 3:
+            robot_speed *= 0.99
+    elif command == keyboard.RIGHT:
+        # print('right')
+        motor_left.setVelocity(robot_speed + 2)
+        motor_right.setVelocity(robot_speed - 2)
+        if abs(robot_speed) > 3:
+            robot_speed *= 0.99
+    else:
+        if command == keyboard.UP:
+            if robot_speed < MAX_SPEED:
+                robot_speed += 0.1
+        elif command == keyboard.DOWN:
+            if robot_speed > -2:
+                robot_speed -= 0.1
+        elif command == 83:  # capture S key
+            robot_speed = 0
+        motor_left.setVelocity(robot_speed)
+        motor_right.setVelocity(robot_speed)
+
+    if command == ord("P"):
+        print(node.getPosition())
